dc59920bd6747576ddce733591c91b078bd64b54
Paweł Pisera <ppisera@e-point.pl>
2020-09-23 10:59:13 +0200

    test

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,71 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="small-container">
+    <employee-form @add:employee="addEmployee" />
+   <employee-table v-bind:employees="employees"  
+          @delete:employee="deleteEmployee"  
+          @edit:employee="editEmployee"
+    />
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import EmployeeTable from '@/components/EmployeeTable.vue'
+import EmployeeForm from '@/components/EmployeeForm.vue'
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    EmployeeTable,
+    EmployeeForm
+    
+  },
+  data() {
+    return {
+      employees: [
+        {
+          id: 1,
+          name: 'ts',
+          email: 'ts@epoint.pl',
+        },
+        {
+          id: 2,
+          name: 'ns',
+          email: 'ns@e-point.pl',
+        },
+        {
+          id: 3,
+          name: 'pp',
+          email: 'pp@e-point.pl',
+        },
+      ],
+    }
+  },
+  methods: {
+    addEmployee(employee) {
+      const lastId = this.employees.length > 0 ? this.employees[this.employees.length - 1].id : 0;
+      const id = lastId + 1;
+      const newEmployee = { ...employee, id };
+      this.employees = [...this.employees, newEmployee];
+    },
+      deleteEmployee(id) {
+    this.employees = this.employees.filter(
+      employee => employee.id !== id
+    )
   }
+  },
+  editEmployee(id, updatedEmployee) {
+  this.employees = this.employees.map(employee =>
+    employee.id === id ? updatedEmployee : employee
+  )
+}
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  button {
+    background: #009435;
+    border: 1px solid #009435;
+  }
+
+  .small-container {
+    max-width: 680px;
+  }
 </style>
